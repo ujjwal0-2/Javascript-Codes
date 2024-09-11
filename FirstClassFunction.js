f16b475c3e9422072090f9f02146413483000abb
UJJWAL SHARMA <92508362+ujjwal0-2@users.noreply.github.com>
2024-09-11 12:13:49 +0530

    Add files via upload

---
+++ FirstClassFunction.js
@@ -0,0 +1,28 @@
+// function statement  aka same // function Expression 
+
+function a(){
+    console.log("ujjwal Sharma");
+}
+
+a();
+
+
+// function declaration
+
+//var b=function (){
+ //   console.log("value");
+//}
+
+
+//  Anonymous function
+//function(){
+    
+//}
+
+// what is callback function 
+setTimeout(function(){
+console.log("a");
+},3000);
+
+
+
